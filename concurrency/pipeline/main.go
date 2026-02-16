030996f014d038db7e1e259f2bf985f7f45d7c8f
Adam <adammertz@gmail.com>
2026-02-16 14:26:07 -0600

    Add `ParseLog` function to extract structured logs using regex

--- concurrency/pipeline/main.go
+++ concurrency/pipeline/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"regexp"
+	"time"
+)
 
 func main() {
 	// start log generator
@@ -25,4 +28,27 @@ type LogRecord struct {
 	Timestamp time.Time
 	Level     LogLevel
 	Message   string
+}
+
+// ParseLog parse a log line into a structured log object
+// Logs are assumed to be in the format <timestamp>|<level>|<message>
+func ParseLog(b []byte) (*LogRecord, error) {
+	logString := string(b)
+	re := regexp.MustCompile(`^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z)\|([A-Z]+)\|(.*)$`)
+	matches := re.FindStringSubmatch(logString)
+	if len(matches) != 4 {
+		// TODO log
+		return nil, nil
+	}
+	record := &LogRecord{}
+	ts, err := time.Parse(time.RFC3339Nano, matches[1])
+	if err != nil {
+		// TODO custom erro
+		return nil, err
+	}
+	record.Timestamp = ts
+	record.Level = LogLevel(matches[2])
+	record.Message = matches[3]
+
+	return record, nil
 }
