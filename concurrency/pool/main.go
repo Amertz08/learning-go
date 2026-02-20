75715fa360b9b199bc2334231aff49c2ca9475e5
Adam <adammertz@gmail.com>
2026-02-20 17:59:15 -0600

    Refactor worker pool to use `Job` struct for enhanced task metadata management and replace `println` with `fmt.Println` for better output formatting

--- concurrency/pool/main.go
+++ concurrency/pool/main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"sync"
 )
 
 func main() {
 	ctx := context.Background()
-	inputChannel := make(chan int)
+	inputChannel := make(chan Job)
 	go func(c context.Context) {
 		defer close(inputChannel)
 		for i := 0; i < 100; i++ {
 			select {
 			case <-c.Done():
 				return
-			case inputChannel <- i:
+			case inputChannel <- Job{Id: i}:
 			}
 		}
 	}(ctx)
@@ -24,18 +25,28 @@ func main() {
 
 	for i := 0; i < maxWorkers; i++ {
 		wg.Add(1)
-		go func(c context.Context, in <-chan int) {
+		go func(c context.Context, in <-chan Job) {
 			defer wg.Done()
 			for input := range in {
 				select {
 				case <-c.Done():
 					return
 				default:
-					println(input)
+					fmt.Println(input)
 				}
 			}
 		}(ctx, inputChannel)
 	}
 
 	wg.Wait()
+}
+
+type Job struct {
+	Id      int
+	Success bool
+	Errors  []error
+}
+
+func (j Job) String() string {
+	return fmt.Sprintf("Job %d: %v", j.Id, j.Success)
 }
