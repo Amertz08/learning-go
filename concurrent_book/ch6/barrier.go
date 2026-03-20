fda6b8ef22226e91877d3e604647d837dc5b5843
Adam <adammertz@gmail.com>
2026-03-20 18:22:52 -0500

    Replace worker example with matrix multiplication using barriers in chapter 6

--- concurrent_book/ch6/barrier.go
+++ concurrent_book/ch6/barrier.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
-	"time"
 )
 
 type Barrier struct {
@@ -32,19 +32,52 @@ func (b *Barrier) Wait() {
 	b.cond.L.Unlock()
 }
 
-func workAndWait(name string, timeToWork int, barr *Barrier) {
-	start := time.Now()
-	for {
-		fmt.Println(time.Since(start).String(), name, "working")
-		time.Sleep(time.Duration(timeToWork) * time.Second)
-		fmt.Println(time.Since(start).String(), name, "waiting on barrier")
-		barr.Wait()
+func main() {
+	var matrixA, matrixB, result [matrixSize][matrixSize]int
+	barrier := NewBarrier(matrixSize + 1)
+	for row := 0; row < matrixSize; row++ {
+		go rowMultiply(&matrixA, &matrixB, &result, row, barrier)
+	}
+
+	for i := 0; i < 4; i++ {
+		generateRandMatrix(&matrixA)
+		generateRandMatrix(&matrixB)
+
+		// Releases the barrier so goroutines can start
+		barrier.Wait()
+
+		// Wait for all goroutines to finish
+		barrier.Wait()
+
+		for i := 0; i < matrixSize; i++ {
+			fmt.Println(matrixA[i], matrixB[i], result[i])
+		}
+		fmt.Println()
 	}
 }
 
-func main() {
-	barrier := NewBarrier(2)
-	go workAndWait("A", 1, barrier)
-	go workAndWait("B", 5, barrier)
-	time.Sleep(20 * time.Second)
+const matrixSize = 3
+
+func generateRandMatrix(matrix *[matrixSize][matrixSize]int) {
+	for row := 0; row < matrixSize; row++ {
+		for col := 0; col < matrixSize; col++ {
+			matrix[row][col] = rand.Intn(10) - 5
+		}
+	}
+}
+
+func rowMultiply(matrixA, matrixB, result *[matrixSize][matrixSize]int, row int, barrier *Barrier) {
+	for {
+		// Wait for all goroutines to reach the barrier before starting
+		barrier.Wait()
+		for col := 0; col < matrixSize; col++ {
+			sum := 0
+			for i := 0; i < matrixSize; i++ {
+				sum += matrixA[row][i] * matrixB[i][col]
+			}
+			result[row][col] = sum
+		}
+		// Wait for rest of goroutines to finish calculating row totals
+		barrier.Wait()
+	}
 }
