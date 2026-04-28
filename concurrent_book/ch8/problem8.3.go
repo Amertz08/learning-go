c24ec76cda602a5da5d0daa2bc2d0992076e3e01
Adam <adammertz@gmail.com>
2026-04-28 14:12:35 -0500

    Add goroutine-based player elimination example for chapter 8 problem 8.3

---
+++ concurrent_book/ch8/problem8.3.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// In the book solution they used an array for the player channels
+	player1 := player()
+	player2 := player()
+	player3 := player()
+	player4 := player()
+
+	playerCount := 4
+
+	for playerCount > 1 {
+		select {
+		case move, open := <-player1:
+			// In the book solution they refactored this logic into a function
+			if !open {
+				player1 = nil
+				playerCount--
+				println("Player 1 closed, player count:", playerCount)
+				continue
+			}
+			println("Player 1 moved:", move)
+		case move, open := <-player2:
+			if !open {
+				player2 = nil
+				playerCount--
+				println("Player 2 closed, player count:", playerCount)
+				continue
+			}
+			println("Player 2 moved:", move)
+		case move, open := <-player3:
+			if !open {
+				player3 = nil
+				playerCount--
+				println("Player 3 closed, player count:", playerCount)
+				continue
+			}
+			println("Player 3 moved:", move)
+		case move, open := <-player4:
+			if !open {
+				player4 = nil
+				playerCount--
+				println("Player 4 closed, player count:", playerCount)
+				continue
+			}
+			println("Player 4 moved:", move)
+		}
+	}
+
+}
+
+func player() chan string {
+	output := make(chan string)
+	count := rand.Intn(100)
+	move := []string{"UP", "DOWN", "LEFT", "RIGHT"}
+	go func() {
+		defer close(output)
+
+		for i := 0; i < count; i++ {
+			output <- move[rand.Intn(4)]
+			d := time.Duration(rand.Intn(200))
+			time.Sleep(d * time.Millisecond)
+		}
+	}()
+
+	return output
+}
