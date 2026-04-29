3ea986f22c7ccc86a0da9aa54c8afb5d83b21e1d
Adam <adammertz@gmail.com>
2026-04-29 15:05:29 -0500

    Add `Drain` function to discard channel messages until quit signal

---
+++ concurrent_book/ch9/problem9.4.go
@@ -0,0 +1,16 @@
+package main
+
+// Drain reads from input channel and discards each message until quit signal is received
+func Drain[T any](quit <-chan int, input <-chan T) {
+	go func() {
+		var msg T
+		for {
+			select {
+			case msg = <-input:
+				continue
+			case <-quit:
+				return
+			}
+		}
+	}()
+}
