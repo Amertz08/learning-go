02b611060a1e5f8f7b66c54ebbd8f2929fba8d9b
Adam <adammertz@gmail.com>
2026-04-29 15:14:42 -0500

    Refactor `TakeUntil` call in `problem9.5.go` by adding `LessThanEqualTo` helper function

--- concurrent_book/ch9/problem9.5.go
+++ concurrent_book/ch9/problem9.5.go
@@ -4,7 +4,11 @@ func main() {
 	quit := make(chan int)
 	Drain[int](quit,
 		Print[int](quit,
-			TakeUntil[int](func(s int) bool { return s <= 1_000 }, quit,
+			TakeUntil[int](LessThanEqualTo(1_000), quit,
 				GenerateSquares(quit))))
 	<-quit
+}
+
+func LessThanEqualTo(s int) func(int) bool {
+	return func(n int) bool { return n <= s }
 }
