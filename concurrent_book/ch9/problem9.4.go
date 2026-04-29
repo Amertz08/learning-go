7b5c0f4002d168347e118d13517bc7eb5e7b488f
Adam <adammertz@gmail.com>
2026-04-29 15:12:39 -0500

    Remove unnecessary variable assignment in `Drain` function

--- concurrent_book/ch9/problem9.4.go
+++ concurrent_book/ch9/problem9.4.go
@@ -3,11 +3,11 @@ package main
 // Drain reads from input channel and discards each message until quit signal is received
 func Drain[T any](quit <-chan int, input <-chan T) {
 	go func() {
-		var msg T
 		for {
 			select {
-			case msg = <-input:
+			case <-input:
 				continue
+
 			case <-quit:
 				return
 			}
