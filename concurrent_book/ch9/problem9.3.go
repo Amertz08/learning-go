1f070d8ceda9f912d1b87130024318b1f66780c5
Adam <adammertz@gmail.com>
2026-04-29 15:00:49 -0500

    Add `Print` function to concurrently process and print channel messages

---
+++ concurrent_book/ch9/problem9.3.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Print reads from input channel and prints each message until quit signal is received
+func Print[T any](quit <-chan int, input <-chan T) <-chan T {
+	output := make(chan T)
+	go func() {
+		defer close(output)
+
+		var msg T
+		moreData := true
+		for moreData {
+			select {
+			case msg, moreData = <-input:
+				fmt.Println(msg)
+				output <- msg
+			case <-quit:
+				return
+			}
+		}
+	}()
+	return output
+}
