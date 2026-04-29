27107c5c44eb7e2808f654e3f34db7fcc877648c
Adam <adammertz@gmail.com>
2026-04-29 15:26:09 -0500

    Refactor `TakeUntil` call in `problem9.5.go` by adding `LessThanEqualTo` helper function

---
+++ concurrency/pipeline_cap/main.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+	The purpose of this program is to demonstrate a pipeline implementation and be a 'capstone' type example project
+
+	Pipeline stages should take a channel as input and output a channel as output
+	1. Generate values
+	2. Implement a fan-out stage to distribute work across multiple goroutines
+	3. Implement a fan-in stage to collect results from multiple goroutines
+	4. Broadcast results to multiple consumers
+*/
+
+func main() {
+}
