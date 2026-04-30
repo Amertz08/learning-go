60c4beef5b44cc3cae490d3e317f8ad4aa8a52aa
Adam <adammertz@gmail.com>
2026-04-29 20:42:29 -0500

    Refactor stack tests to use `NewStack` constructor instead of direct struct initialization

--- datastructures/stack_test.go
+++ datastructures/stack_test.go
@@ -4,20 +4,20 @@ import "testing"
 
 func TestStackImp_Len(t *testing.T) {
 	t.Run("should return 0 for empty stack", func(t *testing.T) {
-		stack := stackImp[int]{}
+		stack := NewStack[int]()
 		if stack.Len() != 0 {
 			t.Errorf("Expected length 0, got %d", stack.Len())
 		}
 	})
 	t.Run("should return correct length after push", func(t *testing.T) {
-		stack := stackImp[int]{}
+		stack := NewStack[int]()
 		stack.Push(1)
 		if stack.Len() != 1 {
 			t.Errorf("Expected length 1, got %d", stack.Len())
 		}
 	})
 	t.Run("should return correct length after multiple pushes", func(t *testing.T) {
-		stack := stackImp[int]{}
+		stack := NewStack[int]()
 		stack.Push(1)
 		stack.Push(2)
 		stack.Push(3)
@@ -29,13 +29,13 @@ func TestStackImp_Len(t *testing.T) {
 
 func TestStackImp_IsEmpty(t *testing.T) {
 	t.Run("should return true for empty stack", func(t *testing.T) {
-		stack := stackImp[int]{}
+		stack := NewStack[int]()
 		if !stack.IsEmpty() {
 			t.Errorf("Expected stack to be empty, got non-empty")
 		}
 	})
 	t.Run("should return false for non-empty stack", func(t *testing.T) {
-		stack := stackImp[int]{}
+		stack := NewStack[int]()
 		stack.Push(1)
 		if stack.IsEmpty() {
 			t.Errorf("Expected stack to be non-empty, got empty")
@@ -45,14 +45,14 @@ func TestStackImp_IsEmpty(t *testing.T) {
 
 func TestStackImp_Pop(t *testing.T) {
 	t.Run("should return false for empty stack", func(t *testing.T) {
-		stack := stackImp[int]{}
+		stack := NewStack[int]()
 		_, ok := stack.Pop()
 		if ok {
 			t.Errorf("Expected false got true for empty stack")
 		}
 	})
 	t.Run("should return true for non empty stack", func(t *testing.T) {
-		stack := stackImp[int]{}
+		stack := NewStack[int]()
 		stack.Push(1)
 		val, ok := stack.Pop()
 		if !ok {
@@ -66,14 +66,14 @@ func TestStackImp_Pop(t *testing.T) {
 
 func TestStackImp_Peak(t *testing.T) {
 	t.Run("returns false on empty stack", func(t *testing.T) {
-		stack := stackImp[int]{}
+		stack := NewStack[int]()
 		_, ok := stack.Peek()
 		if ok {
 			t.Errorf("Expected false got true for empty stack")
 		}
 	})
 	t.Run("returns value at end of stack", func(t *testing.T) {
-		stack := stackImp[int]{}
+		stack := NewStack[int]()
 		stack.Push(1)
 		val, ok := stack.Peek()
 		if !ok {
