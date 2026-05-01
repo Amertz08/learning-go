6fdece6b3dc5e9374b21c31a1d56726ed271f6a6
Adam <adammertz@gmail.com>
2026-05-01 16:34:38 -0500

    Adjust client to actually cause pool exhaustion

--- concurrent_book/ch10/client/main.go
+++ concurrent_book/ch10/client/main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"net"
+	"sync"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		log.Fatalf("error connecting client: %s", err)
-	}
-	defer conn.Close()
+	clientCount := 15
 
-	fmt.Fprintf(conn, "Hello from client\n")
+	var wg sync.WaitGroup
+	for i := 0; i < clientCount; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", "localhost:8080")
+			if err != nil {
+				fmt.Printf("error connecting client: %s", err)
+				return
+			}
+			defer conn.Close()
 
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		fmt.Println("received from server:", scanner.Text())
+			fmt.Fprintf(conn, "Hello from client\n")
+
+			scanner := bufio.NewScanner(conn)
+			for scanner.Scan() {
+				fmt.Println("received from server:", scanner.Text())
+			}
+		}()
 	}
+	wg.Wait()
 }
