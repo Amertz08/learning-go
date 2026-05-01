00bff966cd71d9ad08dd549150dbc17cc1399c23
Adam <adammertz@gmail.com>
2026-04-30 23:10:26 -0500

    Add concurrent file reading example

--- file_processing/reader.go
+++ file_processing/reader.go
@@ -6,12 +6,17 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
+	"time"
 )
 
 const buffSize = 4096
 
 func main() {
-	chunkStreamLargeFile()
+	// TODO: CLI flags to pick a method
+	start := time.Now()
+	chunkConcurrentStreamLargeFile()
+	fmt.Println("took:", time.Since(start))
 }
 
 func readSmallFile() {
@@ -61,4 +66,40 @@ func chunkStreamLargeFile() {
 			log.Fatalf("error reading into buffer: %s", err)
 		}
 	}
+}
+
+func chunkConcurrentStreamLargeFile() {
+	file, err := os.Open("file_processing/large_file.bin")
+	if err != nil {
+		log.Fatalf("error reading large file: %s", err)
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatalf("error getting file stats: %s", err)
+	}
+
+	fileSize := stat.Size()
+	// TODO: I think we also need to do mod := fileSize % buffSize then if mod != 0 -> chunkCount++
+	//		Because if there is a remainder there is 1 more chunk left
+	//		I generated a 1GB file and thus no remainder
+	chunkCount := fileSize / buffSize
+
+	var wg sync.WaitGroup
+	for i := int64(0); i < chunkCount; i++ {
+		wg.Add(1)
+		go func(offset int64) {
+			defer wg.Done()
+			buf := make([]byte, buffSize)
+
+			_, err := file.ReadAt(buf, offset)
+			if err != nil {
+				log.Println("error read at: %s", err)
+				return
+			}
+			fmt.Println(string(buf))
+		}(buffSize * i)
+	}
+	wg.Wait()
 }
