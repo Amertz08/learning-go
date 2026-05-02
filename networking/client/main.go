019389e545159f95b1ce14653dfe7333d34f691b
Adam <adammertz@gmail.com>
2026-05-02 18:58:32 -0500

    Adjust client to not open connection until input is taken

--- networking/client/main.go
+++ networking/client/main.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
 
 	var msg string
 	for {
+		fmt.Print("what to send?: ")
+		scanner := bufio.NewScanner(os.Stdin)
+
 		conn, err := net.Dial("tcp", ":8080")
 		if err != nil {
 			log.Fatalf("encountered an error trying to connect: %s", err)
 		}
 		defer conn.Close()
-		fmt.Print("what to send?: ")
-		fmt.Scanf("%s", &msg)
-		conn.Write([]byte(msg))
 
+		if scanner.Scan() {
+			msg = scanner.Text()
+			fmt.Printf("input: %s\n", msg)
+			conn.Write([]byte(msg))
+		}
 	}
 
 }
