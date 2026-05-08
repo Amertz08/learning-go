1fcb62f08576495dde2fd57e3f968b933de71513
Adam <adammertz@gmail.com>
2026-05-08 07:28:26 -0700

    Refactor into helper

--- api/api_test.go
+++ api/api_test.go
@@ -21,7 +21,7 @@ var _ = Describe("Interacting with API", func() {
 		server = newServer("localhost", "8080")
 	})
 	It("has valid request", func() {
-		request, _ := http.NewRequest("GET", "/", nil)
+		request := newGETRequest("/", nil)
 		recorder := httptest.NewRecorder()
 
 		server.Handler.ServeHTTP(recorder, request)
@@ -30,3 +30,13 @@ var _ = Describe("Interacting with API", func() {
 		Expect(body).To(Equal("hello"))
 	})
 })
+
+func newGETRequest(path string, params map[string]string) *http.Request {
+	request, _ := http.NewRequest("GET", path, nil)
+	q := request.URL.Query()
+	for k, v := range params {
+		q.Add(k, v)
+	}
+	request.URL.RawQuery = q.Encode()
+	return request
+}
