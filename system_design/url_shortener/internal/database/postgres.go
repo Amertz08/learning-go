b12a2696fe93d188a54759ccba954fe1ce8d2b1e
Adam <adammertz@gmail.com>
2026-05-11 13:46:49 -0500

    First pass implementation of CreatedShortenedRecord

--- system_design/url_shortener/internal/database/postgres.go
+++ system_design/url_shortener/internal/database/postgres.go
@@ -19,7 +19,27 @@ func (s *PGDataStore) CreateShortenedRecord(
 	ctx context.Context,
 	encoded, targetUrl string,
 ) (*server.ShortenedRecord, error) {
-	return nil, nil
+	args := pgx.NamedArgs{
+		"encoded":    encoded,
+		"target_url": targetUrl,
+	}
+	rows, err := s.db.Query(ctx, `
+INSERT INTO shortened (encoded, target_url)
+VALUES (@encoded, @target_url)
+RETURNING id, created_at
+;
+`, args)
+	if err != nil {
+		return nil, err
+	}
+	record := &server.ShortenedRecord{
+		Encoded:   encoded,
+		TargetURL: targetUrl,
+	}
+	if err = rows.Scan(&record.Id, &record.CreatedAt); err != nil {
+		return nil, err
+	}
+	return record, nil
 }
 
 func (s *PGDataStore) Get(context.Context, string) (*server.ShortenedRecord, error) {
