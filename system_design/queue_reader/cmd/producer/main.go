04eb0b4ffaef472c7425b9abe9d36b23f7455239
Adam <adammertz@gmail.com>
2026-05-13 15:01:56 -0500

    Write very basic producer

---
+++ system_design/queue_reader/cmd/producer/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println("error dialing rabbitmq")
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println("error creating channel")
+		os.Exit(1)
+	}
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		true,    // durability
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		amqp.Table{
+			amqp.QueueTypeArg: amqp.QueueTypeQuorum,
+		},
+	)
+	if err != nil {
+		fmt.Println("error creating queue")
+		os.Exit(1)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "hello"
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+}
