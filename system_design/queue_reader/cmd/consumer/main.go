02e61dda83923d2a77ddf4d1c630af4129ca1834
Adam <adammertz@gmail.com>
2026-05-13 15:45:12 -0500

    Start rabbitmq implementation

--- system_design/queue_reader/cmd/consumer/main.go
+++ system_design/queue_reader/cmd/consumer/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
 	"sync"
-	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -16,63 +13,108 @@ func main() {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	var queue QueueReader[int]
+	defer queue.Close()
+
+	var wg sync.WaitGroup
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			select {
+			case <-ctx.Done():
+				return
+			case _, ok := <-queue.Read(ctx):
+				if !ok {
+					return
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+type QueueReader[T any] interface {
+	Close() error
+	Publish(ctx, val T) error
+	Read(ctx context.Context) <-chan T
+}
+
+type EncodeDecoder[T any] interface {
+	Encode(T any) error
+	Decode(val any) (T, error)
+}
+
+type RabbitMQImpl[T any] struct {
+	conn          *amqp.Connection
+	channel       *amqp.Channel
+	name          string
+	encodeDecoder EncodeDecoder[T]
+}
+
+func NewRabbitMQImpl[T any]() (*RabbitMQImpl[T], error) {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
-		fmt.Println("failed to connect to queue")
-		os.Exit(1)
+		return nil, err
 	}
-	defer conn.Close()
 
 	ch, err := conn.Channel()
 	if err != nil {
-		fmt.Println("failed to open channel")
-		os.Exit(1)
+		return nil, err
 	}
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		true,    // durability
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		amqp.Table{
-			amqp.QueueTypeArg: amqp.QueueTypeQuorum,
-		},
-	)
-	if err != nil {
-		fmt.Println("failed to create queue")
-		os.Exit(1)
+	q := &RabbitMQImpl[T]{
+		conn:    conn,
+		channel: ch,
+		name:    "hello",
 	}
+	return q, nil
+}
 
-	msgs, err := ch.ConsumeWithContext(
-		ctx,
-		q.Name, // queue
+func (q *RabbitMQImpl[T]) Close() error {
+	if err := q.conn.Close(); err != nil {
+		return err
+	}
+	if err := q.channel.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (q *RabbitMQImpl[T]) Read(ctx context.Context) <-chan T {
+	msgs, err := q.channel.ConsumeWithContext(ctx,
+		q.name, // queue
 		"",     // consumer
 		true,   // auto-ack
 		false,  // exclusive
 		false,  // no-local
 		false,  // no-wait
-		nil,    // args
+		nil,
 	)
 
-	var wg sync.WaitGroup
-	for i := 0; i < workers; i++ {
-		wg.Add(1)
-		go func(id int) {
-			defer wg.Done()
-			for {
-				select {
-				case <-ctx.Done():
-				case msg, ok := <-msgs:
-					if !ok {
-						return
-					}
-					fmt.Println(id, msg)
-					time.Sleep(1 * time.Second)
-				}
-			}
-		}(i)
+	if err != nil {
+		return nil
 	}
-	wg.Wait()
+
+	output := make(chan T)
+
+	go func() {
+		defer close(output)
+		for {
+			select {
+			case <-ctx.Done():
+				break
+			case msg, ok := <-msgs:
+				if !ok {
+					break
+				}
+				val, decodeErr := q.encodeDecoder.Decode(msg)
+				if decodeErr != nil {
+					// TODO: do something
+					continue
+				}
+				output <- val
+			}
+		}
+	}()
+	return output
 }
