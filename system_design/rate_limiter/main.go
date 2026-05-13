a487f14f54d1da2b840e9753c603188c387fc28d
Adam <adammertz@gmail.com>
2026-05-12 23:26:36 -0500

    Implement TokenLimiter

--- system_design/rate_limiter/main.go
+++ system_design/rate_limiter/main.go
@@ -8,6 +8,53 @@ import (
 	"time"
 )
 
+type tokens chan struct{}
+
+type TokenLimiter struct {
+	count  int
+	tokens tokens
+	ticker *time.Ticker
+}
+
+func NewTokenLimiter(count int) *TokenLimiter {
+	// TODO: configurable timer
+	toks := make(chan struct{}, count)
+	for i := 0; i < count; i++ {
+		toks <- struct{}{}
+	}
+
+	return &TokenLimiter{
+		count:  count,
+		tokens: toks,
+		ticker: time.NewTicker(2 * time.Second),
+	}
+}
+
+func (l *TokenLimiter) Start(ctx context.Context) {
+	go func() {
+		for {
+			select {
+			case <-l.ticker.C:
+				select {
+				case l.tokens <- struct{}{}:
+				default:
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+}
+
+func (l *TokenLimiter) Acquire() bool {
+	select {
+	case <-l.tokens:
+		return true
+	default:
+		return false
+	}
+}
+
 func main() {
 	// script showing bucket rate limiting but how do I integrate this into an API?
 	// TODO: use timer
