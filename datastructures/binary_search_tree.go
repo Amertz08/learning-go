0d0347455409b612370d0326b9b38604375db9b3
Adam <adammertz@gmail.com>
2026-05-18 10:52:54 -0500

    Start BST node implementation

--- datastructures/binary_search_tree.go
+++ datastructures/binary_search_tree.go
@@ -39,3 +39,31 @@ type BSTNode[T constraints.Ordered] interface {
 	HasLeft() bool
 	HasRight() bool
 }
+
+type bstNodeImpl[T constraints.Ordered] struct {
+	val T
+}
+
+func NewBSTNodeImpl[T constraints.Ordered](val T) BSTNode[T] {
+	return &bstNodeImpl[T]{val}
+}
+
+func (n *bstNodeImpl[T]) Value() T {
+	return n.val
+}
+
+func (n *bstNodeImpl[T]) Left() (BSTNode[T], bool) {
+	return nil, false
+}
+
+func (n *bstNodeImpl[T]) Right() (BSTNode[T], bool) {
+	return nil, false
+}
+
+func (n *bstNodeImpl[T]) HasLeft() bool {
+	return false
+}
+
+func (n *bstNodeImpl[T]) HasRight() bool {
+	return false
+}
---
+++ datastructures/binary_search_tree_test/node_test.go
@@ -0,0 +1,51 @@
+package binary_search_tree_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.come/Amertz08/learning-go/datastructures"
+)
+
+func TestBST(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "BST tests")
+}
+
+var _ = Describe("interacting with BST node", func() {
+	When("it has a value", func() {
+		It("can return the value", func() {
+			val := 1
+			n := datastructures.NewBSTNodeImpl[int](val)
+
+			Expect(n.Value()).To(Equal(val))
+		})
+		It("will return false when accessing left", func() {
+			val := 1
+			n := datastructures.NewBSTNodeImpl[int](val)
+
+			_, ok := n.Left()
+			Expect(ok).To(BeFalse())
+		})
+		It("will return false when accessing right", func() {
+			val := 1
+			n := datastructures.NewBSTNodeImpl[int](val)
+
+			_, ok := n.Right()
+			Expect(ok).To(BeFalse())
+		})
+		It("will return false when using HasLeft", func() {
+			val := 1
+			n := datastructures.NewBSTNodeImpl[int](val)
+
+			Expect(n.HasLeft()).To(BeFalse())
+		})
+		It("will return false when using HasRight", func() {
+			val := 1
+			n := datastructures.NewBSTNodeImpl[int](val)
+
+			Expect(n.HasRight()).To(BeFalse())
+		})
+	})
+})
