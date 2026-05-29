6df704e5c9bdb6730f4c92f2c261d83e6f71a430
Adam <adammertz@gmail.com>
2026-05-29 12:20:28 -0500

    Start min heap implementation

--- datastructures/heap.go
+++ datastructures/heap.go
@@ -11,3 +11,25 @@ type Heap[T constraints.Ordered] interface {
 // left child 2 * i
 // right child 2 * i + 1
 // parent i / 2
+
+func NewMinHeap[T constraints.Ordered]() Heap[T] {
+	var zeroVal T
+	return &minHeapImpl[T]{data: []T{zeroVal}}
+}
+
+type minHeapImpl[T constraints.Ordered] struct {
+	data []T
+}
+
+func (h *minHeapImpl[T]) Add(value T) {
+
+}
+
+func (h *minHeapImpl[T]) Pop() T {
+	var zeroVal T
+	return zeroVal
+}
+
+func (h *minHeapImpl[T]) List() []T {
+	return h.data
+}
