e49f5bf08fc96325c4222263c21099e50879755c
Adam <adammertz@gmail.com>
2026-05-29 10:34:53 -0500

    Start heap interface

---
+++ datastructures/heap.go
@@ -0,0 +1,12 @@
+package datastructures
+
+import "golang.org/x/exp/constraints"
+
+type Heap[T constraints.Ordered] interface {
+	Add(value T)
+	Pop() T
+}
+
+// left child 2 * i
+// right child 2 * i + 1
+// parent i / 2
