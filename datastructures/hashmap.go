c201a80f65cc83be680271c629301ef5959ae43c
Adam <adammertz@gmail.com>
2026-06-06 12:55:48 -0500

    Kinda start implementation

--- datastructures/hashmap.go
+++ datastructures/hashmap.go
@@ -1,6 +1,41 @@
 package datastructures
 
 type HashMap[T any] interface {
-	Put(key string, value T)
+	Put(key string, value T) error
 	Get(key string) T
+}
+
+type hashValue[T any] struct {
+	key   string
+	value T
+}
+
+type basicHashImpl[T any] struct {
+	data     []hashValue[T]
+	size     int
+	capacity int
+}
+
+func CreateHashMap[T any]() HashMap[T] {
+	default_cap := 2
+	return &basicHashImpl[T]{data: make([]hashValue[T], default_cap), size: 0, capacity: default_cap}
+}
+
+func (h *basicHashImpl[T]) Put(key string, value T) error {
+	//hashInt, err := strconv.Atoi(key)
+	//if err != nil {
+	//	return err
+	//}
+	//idx := hashInt % h.capacity
+	//data := hashValue[T]{
+	//	key:   key,
+	//	value: value,
+	//}
+	//h.data[idx] = data
+	return nil
+}
+
+func (h *basicHashImpl[T]) Get(key string) T {
+	var zeroVal T
+	return zeroVal
 }
