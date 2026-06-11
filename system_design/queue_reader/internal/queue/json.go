65e87aff0b151ecfb555188da09a04e038936d3f
Adam <adammertz@gmail.com>
2026-06-11 09:09:36 -0500

    Don't export the actual implementation 2

--- system_design/queue_reader/internal/queue/json.go
+++ system_design/queue_reader/internal/queue/json.go
@@ -5,14 +5,14 @@ import (
 	"encoding/json"
 )
 
-type JSONEncodeDecoder[T any] struct {
+type jsonEncodeDecoder[T any] struct {
 }
 
-func NewJSONEncodeDecoder[T any]() *JSONEncodeDecoder[T] {
-	return &JSONEncodeDecoder[T]{}
+func NewJSONEncodeDecoder[T any]() *jsonEncodeDecoder[T] {
+	return &jsonEncodeDecoder[T]{}
 }
 
-func (e *JSONEncodeDecoder[T]) Encode(val T) ([]byte, error) {
+func (e *jsonEncodeDecoder[T]) Encode(val T) ([]byte, error) {
 	var buff bytes.Buffer
 	if err := json.NewEncoder(&buff).Encode(&val); err != nil {
 		return nil, err
@@ -20,7 +20,7 @@ func (e *JSONEncodeDecoder[T]) Encode(val T) ([]byte, error) {
 	return buff.Bytes(), nil
 }
 
-func (e *JSONEncodeDecoder[T]) Decode(val []byte) (T, error) {
+func (e *jsonEncodeDecoder[T]) Decode(val []byte) (T, error) {
 	var d T
 	if err := json.NewDecoder(bytes.NewReader(val)).Decode(&d); err != nil {
 		return d, err
